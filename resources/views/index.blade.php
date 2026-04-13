cb57b400a8252cf6d2dfe97f7211adc8866188e4
edwardevok <edulopez4815@gmail.com>
2026-04-12 23:04:28 -0300

    Cambio de tipografia de texto y titulos (no definitiva)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -105,7 +108,7 @@
                 
                 {{-- Texto de Presentación centrado --}}
                 <div class="presentation-text text-center">
-                    <p class="lead" style="color: #4a5568; line-height: 1.8; margin-bottom: 0;">
+                    <p>
                         La Tienda UNNE nace para fortalecer el sentido de pertenencia de toda la comunidad universitaria,
                         ofreciendo productos que reflejan nuestra identidad. Como universidad a la vanguardia en
                         tecnología y desarrollo sustentable, cada artículo representa innovación, compromiso y cuidado
@@ -130,7 +133,7 @@
         <div class="row g-4">
 
             <div class="col-12 col-md-6 col-lg-3">
-    <div class="card h-100 shadow-sm border-0">
+    <div class="card h-100 border-0">
         
         <div class="text-center pt-4">
             <span class="material-symbols-rounded" style="font-size: 80px; color: #021A54;">
@@ -146,7 +149,7 @@
 </div>
 
              <div class="col-12 col-md-6 col-lg-3">
-    <div class="card h-100 shadow-sm border-0">
+    <div class="card h-100 border-0">
         
         <div class="text-center pt-4">
             <span class="material-symbols-rounded" style="color: #021A54; font-size: 80px;">personal_bag</span>
@@ -160,7 +163,7 @@
 </div>
 
              <div class="col-12 col-md-6 col-lg-3">
-    <div class="card h-100 shadow-sm border-0">
+    <div class="card h-100 border-0">
         
         <div class="text-center pt-4">
             <span class="material-symbols-rounded" style="font-size: 80px; color: #021A54;">
@@ -176,7 +179,7 @@
 </div>
 
              <div class="col-12 col-md-6 col-lg-3">
-    <div class="card h-100 shadow-sm border-0">
+    <div class="card h-100 border-0">
         
         <div class="text-center pt-4">
             <span class="material-symbols-rounded" style="font-size: 80px; color: #021A54;">
